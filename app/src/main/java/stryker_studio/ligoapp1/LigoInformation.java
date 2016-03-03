14ac29be23a60c4f7c93a80721b062d69c088308
Danny Truong <dannyalmighty@icloud.com>
2016-03-02 23:01:57 -0600

    All FLoating action buttons work on all pages.  Need to add on click listener then youtube links

--- app/src/main/java/stryker_studio/ligoapp1/LigoInformation.java
+++ app/src/main/java/stryker_studio/ligoapp1/LigoInformation.java
@@ -1,8 +1,10 @@
 package stryker_studio.ligoapp1;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
@@ -10,10 +12,15 @@ import butterknife.ButterKnife;
  */
 public class LigoInformation extends AppCompatActivity{
 
+    @Bind(R.id.youtube_button) FloatingActionButton youtube_button;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ligo_information);
         ButterKnife.bind(this);
+
+
+
 
     }
 
